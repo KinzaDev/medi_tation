45b8c1025da78828dcc79a42b02e307e5e7f2dc0
IRFAN\irfan <kinza.dev1@gmail.com>
2026-02-27 01:35:24 +0500

    SignIn_SignUp page is  created

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -12,7 +12,7 @@ class MeditationApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SigninSignupPage(), // 👈 call your page here
+      home: SigninSignupPage(), //  call your page here
     );
   }
 }
